c1bcb00ea3e62bd7dcad3729d73817c1966382cd
Danillo <danillodana@outlook.com>
2023-06-01 03:26:05 -0300

    Buscar eventos, Participantes do evento, Gerar CSV

--- eventos/views.py
+++ eventos/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, redirect, reverse, HttpResponse, get_object
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.http import Http404
 from .models import Evento
+import csv
+from secrets import token_urlsafe
+import os
+from django.conf import settings
 
 @login_required(login_url='/auth/login')
 def novo_evento(request):
@@ -38,7 +43,15 @@ def novo_evento(request):
         messages.add_message(request, constants.SUCCESS, 'Evento cadastrado com sucesso!')
         return redirect('/eventos/gerenciar_evento')
 
-#TODO criar def buscar eventos | copiar gerenciar eventos, mas excluir a linha 49 eventos = Evento.objects.filter(criador=request.user)
+@login_required(login_url='/auth/login')
+def buscar_evento(request):
+    if request.method == "GET":
+        nome = request.GET.get('nome')
+        eventos = Evento.objects.all()
+        if nome:
+            eventos = eventos.filter(nome__contains=nome)
+
+        return render(request, 'buscar_evento.html', {'eventos': eventos})
 
 @login_required(login_url='/auth/login')
 def gerenciar_evento(request):
@@ -63,3 +76,34 @@ def inscrever_evento(request, id):
 
         messages.add_message(request, constants.SUCCESS, 'Pronto! Sua inscrição já está garantida.')
         return redirect(reverse('inscrever_evento', kwargs={'id': id}))
+
+@login_required(login_url='/auth/login')
+def participantes_evento(request, id):
+    evento = get_object_or_404(Evento, id=id)
+
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu!')
+
+    if request.method == "GET":
+        participantes = evento.participantes.all()
+        return render(request, 'participantes_evento.html', {'evento': evento, 'participantes': participantes})
+
+@login_required(login_url='/auth/login')
+def gerar_csv(request, id):
+    evento = get_object_or_404(Evento, id=id)
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu!')
+
+    participantes = evento.participantes.all()
+
+    token = f'{token_urlsafe(6)}.csv'
+    path = os.path.join(settings.MEDIA_ROOT, token)
+
+    with open(path, 'w') as arq:
+        writer = csv.writer(arq, delimiter=",")
+        for participante in participantes:
+            x=(participante.username, participante.email)
+            writer.writerow(x)
+
+    return redirect(f'/media/{token}')
+    
--- readme.md
+++ readme.md
@@ -2,8 +2,9 @@
 
 uma aplicação para criação de eventos feita com python e django
 
-**Fiz a parte de gerenciar eventos e se inscrever em algum! Encontrei alguns problemas no projeto, amanhã vou arrumar e deixar organizado
+# Criei a pagina de buscar por um evento especifico, ajustei configs do layout de algumas paginas com tabelas, além disso criei a pagina de lista de participantes com opcao de gerar um CSV com todos os participantes!
 
 * Adicionei verificação de senha com regex ao cadastro
+* Geração de CSV
 
 Feita junto ao curso PythonFull
